4dfed3fa582fa06aff057f4401765ecd5b6ae3ce
Soomin Song <d.song.149@gmail.com>
2025-05-14 12:28:29 -0700

    README added

--- Program.cs
+++ Program.cs
@@ -10,17 +10,22 @@
 CallerQueue queue = new CallerQueue();
 queue.Enqueue("Alpha");
 queue.Enqueue("Bravo");
+Console.WriteLine("\nInitialized and enqueued 2 new items.");
 foreach(CallerNode node in queue) { Console.WriteLine(node.ToString()); }
 
 queue.Enqueue("Charlie");
 queue.Enqueue("Delta");
 queue.Enqueue("Echo");
 queue.Dequeue();
+Console.WriteLine("\nDequeued once and enqueued 3 new items.");
 foreach(CallerNode node in queue) { Console.WriteLine(node.ToString()); }
 
 queue.Enqueue("Foxtrot");
 queue.Dequeue();
+Console.WriteLine("\nEnqueued once and dequeued once. Order of operation doesn't matter if queue size is large enough.");
 foreach(CallerNode node in queue) { Console.WriteLine(node.ToString()); }
+
+Console.ReadKey();
 
 
 class CallerQueue: IEnumerable<CallerNode>
